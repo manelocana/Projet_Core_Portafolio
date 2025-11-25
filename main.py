23bc6f625332cf772d9940a0a5cd5d36abd7db9c
manelocana <manelocana.dev@gmail.com>
2025-11-25 10:58:45 +0100

    dynamic header for flask, corrections link style for flask, correction load img for flask,

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/pages")
+def pages():
+    return render_template("pages.html")
+
+@app.route("/portfolio")
+def portfolio():
+    return render_template("portfolio.html")
+
+@app.route("/blog")
+def blog():
+    return render_template("blog.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 
 if __name__ == "__main__":
