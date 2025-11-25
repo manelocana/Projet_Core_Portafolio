6bee04d10fd7899ad7fd18c2ab5731ad80b81c2f
manelocana <manelocana.dev@gmail.com>
2025-11-25 21:19:23 +0100

    change structure of ALL html for play with flask, modified main too for dynamic button search in  header

--- main.py
+++ main.py
@@ -6,23 +6,23 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", title='Home', search_button=True)
 
 @app.route("/pages")
 def pages():
-    return render_template("pages.html")
+    return render_template("pages.html", search_button=True)
 
 @app.route("/portfolio")
 def portfolio():
-    return render_template("portfolio.html")
+    return render_template("portfolio.html", search_button=True)
 
 @app.route("/blog")
 def blog():
-    return render_template("blog.html")
+    return render_template("blog.html", search_button=True)
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", search_button=False)
 
 
 if __name__ == "__main__":
